df314839951632ae6946456b23ddf215ca82380b
mdriyadmr968 <mdriyadmr968@gmail.com>
2021-11-29 20:38:20 +0600

    ui updated

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD3t6iYvxMaY0ccP1a9lhEPmcegBsfvSUs",
+  authDomain: "portfolio-802cb.firebaseapp.com",
+  projectId: "portfolio-802cb",
+  storageBucket: "portfolio-802cb.appspot.com",
+  messagingSenderId: "559874890851",
+  appId: "1:559874890851:web:5b054f03d778e54adf2c56"
+};
+export default firebaseConfig;
